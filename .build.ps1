aa93e6639f924d847c4024cea92d7c491238586a
horker <horker@users.noreply.github.com>
2018-03-30 22:11:31 +0900

    Add the Compile task to compile C# code

--- .build.ps1
+++ .build.ps1
@@ -1,9 +1,29 @@
-task . Build, BuildHelp, ImportDebug
+task . Compile, Build, BuildHelp, ImportDebug
 
 Set-StrictMode -Version 4
 
-# a bug-fixed version to handle errors correctly
-function Copy-ItemError {
+############################################################
+
+$SourcePath = "$PSScriptRoot\cs\dataquery"
+$ModulePath = "$PSScriptRoot\HorkerDataQuery"
+$DebugModulePath = "$PSScriptRoot\debug\HorkerDataQuery"
+
+############################################################
+
+function New-FolderSkip {
+  param(
+    [string]$Path
+  )
+
+  try {
+    New-Item -Type Directory $Path -EA Stop
+  }
+  catch {
+    Write-Host -ForegroundColor DarkCyan $_
+  }
+}
+
+function Copy-ItemSkip {
   param(
     [string]$Source,
     [string]$Dest
@@ -13,38 +33,42 @@ function Copy-ItemError {
     Copy-Item $Source $Dest
   }
   catch {
-    Write-Error $_
+    Write-Host -ForegroundColor DarkCyan $_
   }
 }
 
-$ModulePath = "$PSScriptRoot\HorkerDataQuery"
-$DebugModulePath = "$PSScriptRoot\debug\HorkerDataQuery"
+############################################################
+
+task Compile {
+  msbuild "$SourcePath\dataquery.sln" /p:Configuration=Debug /nologo /v:minimal
+  msbuild "$SourcePath\dataquery.sln" /p:Configuration=Release /nologo /v:minimal
+}
 
 task Build {
 
   . {
     $ErrorActionPreference = "Continue"
 
-    $void = mkdir $ModulePath
-    $void = mkdir "$ModulePath\x64"
-    $void = mkdir "$ModulePath\x86"
+    New-FolderSkip $ModulePath
+    New-FolderSkip "$ModulePath\x64"
+    New-FolderSkip "$ModulePath\x86"
 
-    Copy-ItemError "$PSScriptRoot\scripts\*" $ModulePath
-    Copy-ItemError "$PSScriptRoot\cs\dataquery\bin\Release\Horker.Data.dll" $ModulePath
-    Copy-ItemError "$PSScriptRoot\cs\dataquery\bin\Release\System.Data.SQLite.dll" $ModulePath
-    Copy-ItemError "$PSScriptRoot\cs\dataquery\bin\Release\x64\*" "$ModulePath\x64"
-    Copy-ItemError "$PSScriptRoot\cs\dataquery\bin\Release\x86\*" "$ModulePath\x86"
+    Copy-ItemSkip "$PSScriptRoot\scripts\*" $ModulePath
+    Copy-ItemSkip "$PSScriptRoot\cs\dataquery\bin\Release\Horker.Data.dll" $ModulePath
+    Copy-ItemSkip "$PSScriptRoot\cs\dataquery\bin\Release\System.Data.SQLite.dll" $ModulePath
+    Copy-ItemSkip "$PSScriptRoot\cs\dataquery\bin\Release\x64\*" "$ModulePath\x64"
+    Copy-ItemSkip "$PSScriptRoot\cs\dataquery\bin\Release\x86\*" "$ModulePath\x86"
 
-    $void = mkdir "debug"
-    $void = mkdir $DebugModulePath
-    $void = mkdir "$DebugModulePath\x64"
-    $void = mkdir "$DebugModulePath\x86"
+    New-FolderSkip "debug"
+    New-FolderSkip $DebugModulePath
+    New-FolderSkip "$DebugModulePath\x64"
+    New-FolderSkip "$DebugModulePath\x86"
 
-    Copy-ItemError "$PSScriptRoot\scripts\*" $DebugModulePath
-    Copy-ItemError "$PSScriptRoot\cs\dataquery\bin\Debug\Horker.Data.dll" $DebugModulePath
-    Copy-ItemError "$PSScriptRoot\cs\dataquery\bin\Debug\System.Data.SQLite.dll" $DebugModulePath
-    Copy-ItemError "$PSScriptRoot\cs\dataquery\bin\Debug\x64\*" "$DebugModulePath\x64"
-    Copy-ItemError "$PSScriptRoot\cs\dataquery\bin\Debug\x86\*" "$DebugModulePath\x86"
+    Copy-ItemSkip "$PSScriptRoot\scripts\*" $DebugModulePath
+    Copy-ItemSkip "$PSScriptRoot\cs\dataquery\bin\Debug\Horker.Data.dll" $DebugModulePath
+    Copy-ItemSkip "$PSScriptRoot\cs\dataquery\bin\Debug\System.Data.SQLite.dll" $DebugModulePath
+    Copy-ItemSkip "$PSScriptRoot\cs\dataquery\bin\Debug\x64\*" "$DebugModulePath\x64"
+    Copy-ItemSkip "$PSScriptRoot\cs\dataquery\bin\Debug\x86\*" "$DebugModulePath\x86"
   }
 }
 
