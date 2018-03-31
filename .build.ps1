8412de4e2b62cd19c892f941eb4eed872825e7f4
horker <horker@users.noreply.github.com>
2018-03-31 18:48:55 +0900

    Refactoring .build.ps1

--- .build.ps1
+++ .build.ps1
@@ -4,9 +4,17 @@ Set-StrictMode -Version 4
 
 ############################################################
 
-$SourcePath = "$PSScriptRoot\source"
-$ModulePath = "$PSScriptRoot\HorkerDataQuery"
-$DebugModulePath = "$PSScriptRoot\debug\HorkerDataQuery"
+$SOURCE_PATH = "$PSScriptRoot\source"
+$SCRIPT_PATH = "$PSScriptRoot\scripts"
+
+$MODULE_PATH = "$PSScriptRoot\HorkerDataQuery"
+$DEBUG_MODULE_PATH = "$PSScriptRoot\debug\HorkerDataQuery"
+
+$HELP_INPUT =  "$SOURCE_PATH\bin\Release\Horker.Data.dll"
+$HELP_INTERM = "$SOURCE_PATH\bin\Release\Horker.Data.dll-Help.xml"
+$HELP_OUTPUT = "$MODULE_PATH\Horker.Data.dll-Help.xml"
+
+$HELPGEN = "$PSScriptRoot\vendor\XmlDoc2CmdletDoc.0.2.10\tools\XmlDoc2CmdletDoc.exe"
 
 ############################################################
 
@@ -16,7 +24,7 @@ function New-FolderSkip {
   )
 
   try {
-    New-Item -Type Directory $Path -EA Stop
+    $null = New-Item -Type Directory $Path -EA Stop
   }
   catch {
     Write-Host -ForegroundColor DarkCyan $_
@@ -40,8 +48,8 @@ function Copy-ItemSkip {
 ############################################################
 
 task Compile {
-  msbuild "$SourcePath\dataquery.sln" /p:Configuration=Debug /nologo /v:minimal
-  msbuild "$SourcePath\dataquery.sln" /p:Configuration=Release /nologo /v:minimal
+  msbuild "$SOURCE_PATH\dataquery.sln" /p:Configuration=Debug /nologo /v:quiet
+  msbuild "$SOURCE_PATH\dataquery.sln" /p:Configuration=Release /nologo /v:quiet
 }
 
 task Build {
@@ -49,47 +57,41 @@ task Build {
   . {
     $ErrorActionPreference = "Continue"
 
-    New-FolderSkip $ModulePath
-    New-FolderSkip "$ModulePath\x64"
-    New-FolderSkip "$ModulePath\x86"
+    New-FolderSkip "$MODULE_PATH\x64"
+    New-FolderSkip "$MODULE_PATH\x86"
 
-    Copy-ItemSkip "$PSScriptRoot\scripts\*" $ModulePath
-    Copy-ItemSkip "$SourcePath\bin\Release\Horker.Data.dll" $ModulePath
-    Copy-ItemSkip "$SourcePath\bin\Release\System.Data.SQLite.dll" $ModulePath
-    Copy-ItemSkip "$SourcePath\bin\Release\x64\*" "$ModulePath\x64"
-    Copy-ItemSkip "$SourcePath\bin\Release\x86\*" "$ModulePath\x86"
+    Copy-ItemSkip "$SCRIPT_PATH\*" $MODULE_PATH
+    Copy-ItemSkip "$SOURCE_PATH\bin\Release\Horker.Data.dll" $MODULE_PATH
+    Copy-ItemSkip "$SOURCE_PATH\bin\Release\System.Data.SQLite.dll" $MODULE_PATH
+    Copy-ItemSkip "$SOURCE_PATH\bin\Release\x64\*" "$MODULE_PATH\x64"
+    Copy-ItemSkip "$SOURCE_PATH\bin\Release\x86\*" "$MODULE_PATH\x86"
 
-    New-FolderSkip "debug"
-    New-FolderSkip $DebugModulePath
-    New-FolderSkip "$DebugModulePath\x64"
-    New-FolderSkip "$DebugModulePath\x86"
+    New-FolderSkip "$DEBUG_MODULE_PATH\x64"
+    New-FolderSkip "$DEBUG_MODULE_PATH\x86"
 
-    Copy-ItemSkip "$PSScriptRoot\scripts\*" $DebugModulePath
-    Copy-ItemSkip "$SourcePath\bin\Debug\Horker.Data.dll" $DebugModulePath
-    Copy-ItemSkip "$SourcePath\bin\Debug\Horker.Data.pdb" $DebugModulePath
-    Copy-ItemSkip "$SourcePath\bin\Debug\System.Data.SQLite.dll" $DebugModulePath
-    Copy-ItemSkip "$SourcePath\bin\Debug\x64\*" "$DebugModulePath\x64"
-    Copy-ItemSkip "$SourcePath\bin\Debug\x86\*" "$DebugModulePath\x86"
+    Copy-ItemSkip "$SCRIPT_PATH\*" $DEBUG_MODULE_PATH
+    Copy-ItemSkip "$SOURCE_PATH\bin\Debug\Horker.Data.dll" $DEBUG_MODULE_PATH
+    Copy-ItemSkip "$SOURCE_PATH\bin\Debug\Horker.Data.pdb" $DEBUG_MODULE_PATH
+    Copy-ItemSkip "$SOURCE_PATH\bin\Debug\System.Data.SQLite.dll" $DEBUG_MODULE_PATH
+    Copy-ItemSkip "$SOURCE_PATH\bin\Debug\x64\*" "$DEBUG_MODULE_PATH\x64"
+    Copy-ItemSkip "$SOURCE_PATH\bin\Debug\x86\*" "$DEBUG_MODULE_PATH\x86"
   }
 }
-
-$HELP_INPUT =  "$SourcePath\bin\Release\Horker.Data.dll"
-$HELP_INTERM = "$SourcePath\bin\Release\Horker.Data.dll-Help.xml"
-$HELP_OUTPUT = "$ModulePath\Horker.Data.dll-Help.xml"
 
 task BuildHelp `
   -Inputs $HELP_INPUT `
   -Outputs $HELP_OUTPUT `
 {
-  vendor\XmlDoc2CmdletDoc.0.2.10\tools\XmlDoc2CmdletDoc.exe $HELP_INPUT
+  . $HELPGEN $HELP_INPUT
 
-  Copy-Item $HELP_INTERM $ModulePath
+  Copy-Item $HELP_INTERM $MODULE_PATH
 }
 
 task ImportDebug {
-  Import-Module .\debug\HorkerDataQuery -Force
+  Import-Module $DEBUG_MODULE_PATH -Force
 }
 
 task Clean {
-  Remove-Item "$PSScriptRoot\HorkerDataQuery\*" -Force -EA Continue
+  Remove-Item "$MODULE_PATH\*" -Force -Recurse -EA Continue
+  Remove-Item "$DEBUG_MODULE_PATH\*" -Force -Recurse -EA Continue
 }
