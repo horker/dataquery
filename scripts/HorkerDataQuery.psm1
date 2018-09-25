f694aedcf264ab058fa9478a3824324d1b368bd9
horker <horker@users.noreply.github.com>
2018-09-26 05:06:37 +0900

    Remove alias definition from psm1 because it is defined in C# code

--- scripts/HorkerDataQuery.psm1
+++ scripts/HorkerDataQuery.psm1
@@ -2,6 +2,4 @@ Add-Type -Path $PSScriptRoot\System.Data.SQLite.dll
 
 Import-Module $PSScriptRoot\Horker.Data.dll
 
-Set-Alias idq Invoke-DataQuery
-
 Register-DataConfiguration $PSScriptRoot\sqlite.config
